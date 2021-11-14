8ff05955c7a57fdf2acd3e3a5f07545178fc385b
Kevin Lee <kjl001@ucsd.edu>
2021-11-13 22:14:00 -0800

    finish lab

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Lab 8 - Starter
+1) I would manually run tests locally before pushing code because this would allow quicker fixes and an easier time changing code.
+2) No
+3) No, because the message feature would require multiple components to interact with each other, which unit testing cannot test.
+4) Yes, because checking the max message length is a single individual component, which unit testing can test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,22 @@
 // unit.test.js
 
+const { exportAllDeclaration } = require('@babel/types');
+const { TestWatcher } = require('@jest/core');
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test("Check for a valid phone number", () => {
+    expect(functions.isPhoneNumber("323-459-9023")).toBe(true);
+});
+
+test("Check for a valid email", () => {
+    expect(functions.isEmail("kjl001@ucsd.edu")).toBe(true);
+});
+
+test("Check for a weak password", () => {
+    expect(functions.isStrongPassword("a")).toBe(false);
+});
+
+test("Check for an invalid hex color", () => {
+    expect(functions.isHexColor("djkasda")).toBe(false);
+})
